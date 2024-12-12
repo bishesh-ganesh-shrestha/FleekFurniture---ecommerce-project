12d77ddcd8007f4ae7e57838ef479d283ac10ab7
Bishesh Ganesh Shrestha <unikbisheshshrestha@gmail.com>
2024-12-12 10:54:26 +0545

    adds proper indentation

--- includes/register.php
+++ includes/register.php
@@ -1,4 +1,3 @@
-<!-- account page starts-->
 <?php
 $page_title = "Product Details | hello";
 include('header.php');
@@ -8,46 +7,55 @@ include('dbconn.php');
 
 $flag = 0;
 
-$erPhone = $erUser = "";
+$erPhone = $erUser = $erPassword = "";
 try {
   if(isset($_POST['register'])){
-  if (isset($_REQUEST['customer_name'])) $customer_name =  $_REQUEST['customer_name'];
-  if (isset($_REQUEST['customer_mobile'])) $customer_mobile =  $_REQUEST['customer_mobile'];
-  if(isset($customer_mobile)){
-  if(!preg_match("/^98[0-9]{8}$/", $customer_mobile)){
-      $flag = 1;
-      $erPhone = "Enter a valid phone!";
-  }
-}
-  if (isset($_REQUEST['customer_username'])) $customer_username =  $_REQUEST['customer_username'];
+      if (isset($_REQUEST['customer_name'])) $customer_name =  $_REQUEST['customer_name'];
+      if (isset($_REQUEST['customer_mobile'])) $customer_mobile =  $_REQUEST['customer_mobile'];
 
-  $sql = "SELECT * FROM customers WHERE customer_username = '$customer_username'";
-  $query = mysqli_query($conn, $sql);
-
-
-  if(mysqli_num_rows($query) > 0){
-      $flag = 1;
-      $erUser = "Username already exists!";
-  }
-}
-  if (isset($_REQUEST['customer_password'])) $customer_password =  $_REQUEST['customer_password'];
-  if (isset($_REQUEST['customer_name']) && isset($_REQUEST['customer_mobile']) && isset($_REQUEST['customer_username']) &&isset($_REQUEST['customer_password']) && $flag == 0 ) {
-      $sql = "INSERT INTO customers (customer_name, customer_mobile,customer_username, customer_password)
-          VALUES ('$customer_name','$customer_mobile','$customer_username', '$customer_password')";
-
-      if ($conn->query($sql) === TRUE) {
-          echo "New record created successfully";
-          header("location: login.php");
-      } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
+      if(isset($customer_mobile)){
+          if(!preg_match("/^98[0-9]{8}$/", $customer_mobile)){
+              $flag = 1;
+              $erPhone = "Enter a valid phone!";
+          }
       }
 
-      $conn->close();
+      if (isset($_REQUEST['customer_username'])) $customer_username =  $_REQUEST['customer_username'];
+
+      $sql = "SELECT * FROM customers WHERE customer_username = '$customer_username'";
+      $query = mysqli_query($conn, $sql);
+
+      if(mysqli_num_rows($query) > 0){
+          $flag = 1;
+          $erUser = "Username already exists!";
+      }
+
+      if (isset($_REQUEST['customer_password'])) {
+          $customer_password =  $_REQUEST['customer_password'];
+
+          if (strlen($customer_password) < 8) {
+              $flag = 1;
+              $erPassword = "Password must be at least 8 characters long!";
+          }
+      }
+
+      if (isset($_REQUEST['customer_name']) && isset($_REQUEST['customer_mobile']) && isset($_REQUEST['customer_username']) && isset($_REQUEST['customer_password']) && $flag == 0) {
+          $sql = "INSERT INTO customers (customer_name, customer_mobile, customer_username, customer_password)
+              VALUES ('$customer_name', '$customer_mobile', '$customer_username', '$customer_password')";
+
+          if ($conn->query($sql) === TRUE) {
+              echo "New record created successfully";
+              header("location: login.php");
+          } else {
+              echo "Error: " . $sql . "<br>" . $conn->error;
+          }
+
+          $conn->close();
+      }
   }
-  
-  } catch (Throwable $th) {
-      echo 'Transaction error.';
-  }
+} catch (Throwable $th) {
+    echo 'Transaction error.';
+}
 ?>
 
 <div class="account-page">
@@ -69,22 +77,25 @@ try {
                         <input type="text" name="customer_name" placeholder="NAME" required>
                         <input type="text" name="customer_mobile" placeholder="MOBILE" required>
                         <?php
-
-                                if(isset($erPhone)){
-                                    echo "<span>" . $erPhone . "</span>";
-                                }
-
+                            if(isset($erPhone)){
+                                echo "<span>" . $erPhone . "</span>";
+                            }
                         ?>
-                        
+
                         <input type="text" name="customer_username" placeholder="USERNAME" required>
                         <?php
-
-                                if(isset($erUser)){
-                                    echo "<span>" . $erUser . "</span>";
-                                }
-
+                            if(isset($erUser)){
+                                echo "<span>" . $erUser . "</span>";
+                            }
                         ?>
-                         <input type="password" name="customer_password" placeholder="PASSWORD" required>
+
+                        <input type="password" name="customer_password" placeholder="PASSWORD" required>
+                        <?php
+                            if(isset($erPassword)){
+                                echo "<span>" . $erPassword . "</span>";
+                            }
+                        ?>
+
                         <button type="submit" name="register" class="btn">Register</button>
                     </form>
                 </div>
--- includes/user_login.php
+++ includes/user_login.php
@@ -1,33 +1,30 @@
 <?php
-    $erUsername = $erPass = "";
-    if(isset($_POST['login'])){
-        $username = $_POST['lusername'];
-        $pass = $_POST['lpassword'];
+  $erUsername = $erPass = "";
+  if(isset($_POST['login'])){
+    $username = $_POST['lusername'];
+    $pass = $_POST['lpassword'];
 
-        include('dbconn.php');
+    include('dbconn.php');
 
-        $sql = "SELECT * FROM customers WHERE customer_username = '$username' AND customer_password = '$pass' LIMIT 1";
-        $query = mysqli_query($conn, $sql);
+    $sql = "SELECT * FROM customers WHERE customer_username = '$username' AND customer_password = '$pass' LIMIT 1";
+    $query = mysqli_query($conn, $sql);
 
-        if(mysqli_num_rows($query) > 0){
-            foreach($query as $data){
-                $user_id = $data['customer_id'];
-                $user_name = $data['username'];
-                $user_email = $data['email'];
-            }
-
-            if(isset($user_id)){
-                header("location: products.php");
-                echo "Successful login!";
-            }
+    if(mysqli_num_rows($query) > 0){
+        foreach($query as $data){
+            $user_id = $data['customer_id'];
+            $user_name = $data['username'];
+            $user_email = $data['email'];
         }
 
-        else{
-            echo "Login failed!";
-            header("location: login.php");
+        if(isset($user_id)){
+            header("location: products.php");
+            echo "Successful login!";
         }
-
     }
 
-
+    else{
+        echo "Login failed!";
+        header("location: login.php");
+    }
+  }
 ?>
